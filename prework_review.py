d80fed9f25570e789678c6aed08218aca00c9147
Charles Gurley <chuck.gurley@gmail.com>
2023-03-13 15:28:14 -0500

    first commit

---
+++ prework_review.py
@@ -0,0 +1,48 @@
+def hello_name(user_name): 
+ 
+ 
+ def hello_name(user_name):
+    print("Hello" + user_name.title() + "!")
+    
+hello_name("Chuck")
+
+def odd_numbers():
+    for i in range(0,101,2):
+        print(i)
+        
+def odd_numbers():
+    numbers = list(range(0, 101)) 
+    for number in numbers:
+        if number % 2 != 0:
+            print(number) 
+
+
+#q 3
+def max_num_in_list(a_list):
+    max_num +max(a_list)
+    return max_num
+ 
+
+a_list = ([1,2,4,6,8,15])  
+a_list.sort()  
+return a_list[-1]: 
+
+# question 4
+
+def is_leap_year(a_year)
+if a_year %4 
+
+
+
+# Q5
+
+def is_consecutive(a_list):
+    i = 0
+    
+    status = True
+    while i < len(a_list) -1:
+        i += 1
+    else:
+        status = False
+        break
+print(status)    
